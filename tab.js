9e00a5a5cc4b9463f961ee2766610ad18957128d
森川雅也 <>
2023-08-14 18:02:52 +0900

    リファクタリング('is-active'と’$nav.Length'を定数化)

--- tab.js
+++ tab.js
@@ -4,6 +4,8 @@ const $doc = document;
 const $tab = $doc.getElementById('js-tab');
 const $nav = $tab.querySelectorAll('[data-nav]');
 const $content = $tab.querySelectorAll('[data-content]');
+const ACTIVE_CLASS = 'is-active';
+const navLen = $nav.length;
 
 //初期化
 const init = () => {
@@ -21,16 +23,16 @@ const handleClick = (e) => {
 
     //対象外のnav, content全て一旦リセットする
     let index = 0;
-    while(index < $nav.length){
+    while(index < navLen){
         console.log(index);
         $content[index].style.display = 'none';
-        $nav[index].classList.remove('is-active');
+        $nav[index].classList.remove(ACTIVE_CLASS);
         index++;
     }
 
     //対象のコンテンツをアクティブに化する
     $tab.querySelectorAll('[data-content="' + targetVal + '"]')[0].style.display = 'block';//クリックされたnavのデータ属性に応じて対応するコンテンツエリアのDOMを取得して表示させる
-    $nav[targetVal].classList.add('is-active');//DOM属性についているclass属性の一覧を取得して、classListに”is-active”を追加する
+    $nav[targetVal].classList.add(ACTIVE_CLASS);//DOM属性についているclass属性の一覧を取得して、classListに”is-active”を追加する
 
     console.log('$nav[targetVal].classList', $nav[targetVal].classList);
 
@@ -38,7 +40,7 @@ const handleClick = (e) => {
 
 //全nav要素に対して関数を適用・発火
 let index = 0;
-while(index < $nav.length) {
+while(index < navLen) {
     $nav[index].addEventListener('click', (e) => handleClick(e));
     index++;
 }
